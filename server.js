638d7d64de65fd1b319d8e3ae2f1b6af0ced247d
hassanrobleh <hassan.alirobleh@gmail.com>
2022-01-01 00:22:54 +0100

    add middleware flash

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 
+const app = express()
+const session = require('express-session')
 const port = 3000
 
+// Moteur de template
 app.set('view engine', 'ejs')
 
+// Middleware
 app.use('/assets', express.static('public'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+//app.use(express.json())
+//app.use(express.urlencoded({ extended: true }))
 
+app.use(session({
+    secret: 'azazaze',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+
+// Route
 app.get('/', (req, res) => {
-    res.render('pages/index', {message: 'Hello there!'})
+    // Récupérer les infos stocker dans la session
+    // console.log(req.session.error)
+    if(req.session.error) {
+        res.locals.error = req.session.error
+        req.session.error = undefined
+    }
+    res.render('pages/index')
+})
+
+app.post('/', (req, res) => {
+    if(req.body.message === undefined || req.body.message === '') {
+        //res.render('pages/index', {error: 'Vous n\'avez pas entré de message :('})
+        req.session.error = 'Il y a une erreur'
+        res.redirect('/')
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
