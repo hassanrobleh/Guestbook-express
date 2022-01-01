12801011b026d2e67a12a84bc2845db90e128bb3
hassanrobleh <hassan.alirobleh@gmail.com>
2022-01-02 00:44:25 +0100

    add database MySQL

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
 const app = express()
 const session = require('express-session')
+const flash = require('./middleware/flash')
 const port = 3000
 
 // Moteur de template
@@ -11,10 +12,10 @@ app.set('view engine', 'ejs')
 // Middleware
 app.use('/assets', express.static('public'))
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-//app.use(express.json())
-//app.use(express.urlencoded({ extended: true }))
+//app.use(bodyParser.urlencoded({extended: true}))
+//app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
     secret: 'azazaze',
@@ -23,23 +24,29 @@ app.use(session({
     cookie: { secure: false }
 }))
 
+app.use(flash)
+
 
 // Route
 app.get('/', (req, res) => {
     // Récupérer les infos stocker dans la session
     // console.log(req.session.error)
-    if(req.session.error) {
-        res.locals.error = req.session.error
-        req.session.error = undefined
-    }
+    // if(req.session.error) {
+    //     res.locals.error = req.session.error
+    //     req.session.error = undefined
+    // }
+    //console.log(req.session)
     res.render('pages/index')
 })
 
 app.post('/', (req, res) => {
     if(req.body.message === undefined || req.body.message === '') {
         //res.render('pages/index', {error: 'Vous n\'avez pas entré de message :('})
-        req.session.error = 'Il y a une erreur'
+        //req.session.error = 'Il y a une erreur'
+        req.flash('error', 'Vous n\'avez pas posté de message')
         res.redirect('/')
+    } else {
+
     }
 })
 
